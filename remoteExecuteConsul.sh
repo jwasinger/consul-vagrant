e1334352542c1e5c101a7e6d43ec1a4e1c187b8e
Jared Wasinger <j-wasinger@hotmail.com>
2017-06-19 00:30:51 -0700

    more

--- remoteExecuteConsul.sh
+++ remoteExecuteConsul.sh
@@ -1,5 +1,5 @@
-consulServer="consul agent -server -bootstrap-expect=1 -data-dir=/tmp/consul -node=agent-one -bind=172.20.20.10 -config-dir=/etc/consul.d"
+consulServer="touch /vagrant/consul.log && consul agent -server -bootstrap-expect=1 -data-dir=/tmp/consul -node=agent-one -bind=172.20.20.11 -config-dir=/etc/consul.d >> /vagrant/consul.log"
 if [ "$1" == "n1" ]
 then
-	ssh $(vagrant ssh-config $1 | sed '/^[[:space:]]*$/d' |  awk 'NR>1 {print " -o "$1"="$2}') localhost '$consulServer'
+	ssh $(vagrant ssh-config $1 | sed '/^[[:space:]]*$/d' |  awk 'NR>1 {print " -o "$1"="$2}') localhost "$consulServer"
 fi
