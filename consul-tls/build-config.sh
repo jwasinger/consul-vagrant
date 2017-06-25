937d90bbbe387bc7d41e2104139d8204e00f5b32
Jared Wasinger <j-wasinger@hotmail.com>
2017-06-25 03:57:13 -0700

    .

--- consul-tls/build-config.sh
+++ consul-tls/build-config.sh
@@ -1,19 +1,20 @@
-mkdir -p server/consul.d/ssl/CA && mkdir -p client/consul.d/ssl && \
+mkdir -p build/server/consul.d/ssl/CA && mkdir -p build/client/consul.d/ssl && \
 
+cp build/ssl/ca.cert build/server/consul.d/ssl && \
+cp build/ssl/consul.key build/server/consul.d/ssl && \
+cp build/ssl/consul.cert build/server/consul.d/ssl && \
 
 #chmod 0700 server/consul.d/ssl/CA && \
 echo "building CA server config"
-cp -R conf/ssl/* server/consul.d/ssl/CA && \
-cp -R conf/config.json server/consul.d && \
-cp conf/ssl/ca.cert server/consul.d/ssl && \
-cp conf/ssl/consul.key server/consul.d/ssl && \
-cp conf/ssl/consul.cert server/consul.d/ssl && \
+cp -R build/ssl/* build/server/consul.d/ssl/CA && \
+cp -R conf/server-config.json build/server/consul.d/config.json && \
 
 echo "building client config"
-cp conf/ssl/ca.cert client/consul.d/ssl && \
-cp conf/ssl/consul.key client/consul.d/ssl && \
-cp conf/ssl/consul.cert client/consul.d/ssl && \
-cp conf/config.json client/consul.d
+cp build/ssl/ca.cert build/client/consul.d/ssl && \
+cp build/ssl/consul.key build/client/consul.d/ssl && \
+cp build/ssl/consul.cert build/client/consul.d/ssl && \
 
-cp $GOPATH/src/github.com/hashicorp/consul/bin/consul server && \
-cp $GOPATH/src/github.com/hashicorp/consul/bin/consul client
+cp conf/client-config.json build/client/consul.d
+
+cp $GOPATH/src/github.com/hashicorp/consul/bin/consul build/client && \
+cp $GOPATH/src/github.com/hashicorp/consul/bin/consul build/server
--- consul-tls/setup-tls.sh
+++ consul-tls/setup-tls.sh
@@ -1,7 +1,7 @@
-mkdir conf/ssl && \
-cd conf/ssl && \
+mkdir -p build/ssl && \
+cd build/ssl && \
 echo "000a" > serial && \
 touch certindex && \
 openssl req -x509 -newkey rsa:2048 -days 3650 -nodes -out ca.cert -subj '/O=Jared Wasinger/C=US/ST=Oregon/L=Portland/CN=consul' && \
 openssl req -newkey rsa:1024 -nodes -out consul.csr -keyout consul.key -subj '/O=Jared Wasinger/C=US/ST=Oregon/L=Portland/CN=consul' && \
-openssl ca -batch -config ../myca.conf -notext -in consul.csr -out consul.cert
+openssl ca -batch -config ../../conf/myca.conf -notext -in consul.csr -out consul.cert
